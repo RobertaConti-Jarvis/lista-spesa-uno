6f75934564bd06ac47ac53550dea982f3573fdf0
AulicinoADiego <aulicinodiego@gmail.com>
2021-03-02 16:37:07 +0100

    Implementazione Client e Update Server 2

--- lista-spesa-uno-client/src/app/app.component.ts
+++ lista-spesa-uno-client/src/app/app.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { DtoNotifica } from './dto_notifica';
+import { DtoProdotto } from './dto_prodotto';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  prodotto: DtoProdotto = new DtoProdotto;
+  listaSpesa: DtoProdotto[] = [];
+  messaggioNotifica: string;
+
   title = 'lista-spesa-uno-client';
+
+  constructor(private http: HttpClient) { }
+
+  addProdotto() {
+    if (this.prodotto.nome != "") {
+      let dtoP: DtoProdotto = new DtoProdotto();
+      dtoP.nome = this.prodotto.nome;
+      let oss: Observable<DtoNotifica> = this.http
+        .post<DtoNotifica>('http://localhost:8080/aggiungi', dtoP);
+      oss.subscribe(n => this.messaggioNotifica = n.notifica);
+      this.listaSpesa.push(this.prodotto);
+      this.prodotto = new DtoProdotto();
+      this.messaggioNotifica = "";
+    }
+  }
+
+  deleteAll() {
+    let oss: Observable<DtoNotifica> = this.http
+      .get<DtoNotifica>('http://localhost:8080/elimina-tutto');
+    oss.subscribe(n => this.messaggioNotifica = n.notifica);
+    this.listaSpesa.splice(0);
+    this.messaggioNotifica = "";
+  }
+
 }
