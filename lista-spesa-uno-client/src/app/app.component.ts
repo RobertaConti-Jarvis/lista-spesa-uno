87fde6abee1150faa9d83862eb9064494cddcfbd
Roberta Conti <r.conti70@gmail.com>
2021-03-04 11:30:14 +0100

    Commit #1

--- lista-spesa-uno-client/src/app/app.component.ts
+++ lista-spesa-uno-client/src/app/app.component.ts
@@ -10,20 +10,16 @@ import { DtoContenutoDB } from './dto_contenutoDB';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  /*prodotto: DtoProdotto = new DtoProdotto;*/
   prodotto: string;
   listaSpesa: DtoProdotto[] = [];
-  messaggioNotifica: string;
 
   title = 'lista-spesa-uno-client';
-  ButtonImg: boolean = true;
 
   constructor(private http: HttpClient) {
     this.leggiDB();
   }
 
   leggiDB() {
-    this.ButtonImg = false;
     let oss: Observable<DtoContenutoDB> = this.http
       .get<DtoContenutoDB>('http://localhost:8080/carica-dati-da-visualizzare');
     oss.subscribe(d => this.listaSpesa = d.contenutoDB);
